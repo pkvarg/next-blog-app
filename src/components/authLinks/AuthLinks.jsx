9f1a979aaa29d99cb8bde77abf69c58d728f7dc8
peter varga <pkvarg@yahoo.se>
2023-10-09 13:54:16 +0200

    02:42 not logging in

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -7,8 +7,7 @@ import { signOut, useSession } from 'next-auth/react'
 const AuthLinks = () => {
   const [open, setOpen] = useState(false)
 
-  // const { status } = useSession();
-  const { status } = 'authenticated'
+  const { status } = useSession()
 
   return (
     <>
