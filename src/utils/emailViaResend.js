b96b1d4420fb21ce3894f2ac56132ab4d24d57d8
peter varga <pkvarg@yahoo.se>
2023-10-26 05:29:11 +0200

    resend

--- src/utils/emailViaResend.js
+++ src/utils/emailViaResend.js
@@ -1,20 +1,20 @@
 import { Resend } from 'resend'
-import emailTemplate from '@/libs/emailViaResend/emailTemplate'
+import emailTemplate from '@/utils/emailTemplate'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 export default async function EmailViaResend(name, email, message) {
   try {
     await resend.sendEmail({
-      from: 'email@project.pictusweb.com',
+      from: 'bible-blog@online',
       to: email,
       subject: 'Your message',
       react: emailTemplate(name, email, message),
     })
     await resend.sendEmail({
-      from: 'email@project.pictusweb.com',
+      from: 'bible-blog@online',
       to: 'info@pictusweb.sk',
       subject: 'Nová správa Blog',
-      react: emailTemplate(name),
+      react: emailTemplate(name, email, message),
     })
   } catch (error) {
     console.log(error)
